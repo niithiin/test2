dc755f61ab598e2aa7552e106dbce1db07c6dd4e
niithiin <43510450+niithiin@users.noreply.github.com>
2019-02-01 11:16:08 +0530

    Update HelloWorld.java

--- src/oata/HelloWorld.java
+++ src/oata/HelloWorld.java
@@ -2,6 +2,6 @@ package oata;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello nithin hi");
+        System.out.println("Hello globe");
     }
 }
